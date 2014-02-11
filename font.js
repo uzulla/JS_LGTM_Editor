c5e876327c21d4183d09e1222b55604d7851c3f5
uzulla <uzulla@himitsukichi.com>
2014-02-11 21:06:36 +0900

    add font
    request by moznion

--- font.js
+++ font.js
@@ -5,5 +5,7 @@ var fontList = [
     ['Bowlby One SC','http://fonts.googleapis.com/css?family=Bowlby+One+SC'],
     ['Fascinate','http://fonts.googleapis.com/css?family=Fascinate'],
     ['Vibur','http://fonts.googleapis.com/css?family=Vibur'],
-    ['Audiowide','http://fonts.googleapis.com/css?family=Audiowide']
+    ['Audiowide','http://fonts.googleapis.com/css?family=Audiowide'],
+    ['Codystar','http://fonts.googleapis.com/css?family=Codystar'],
+    ['Vast Shadow','http://fonts.googleapis.com/css?family=Vast+Shadow']
 ];
